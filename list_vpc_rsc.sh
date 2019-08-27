51a5b3becefab9fde14cacda60f29b05d5b6282d
mqg <mqg@3ds.com>
2019-08-27 16:42:51 +0530

    updated list_vpc_rsc.sh: listing resources Instance, volume

--- list_vpc_rsc.sh
+++ list_vpc_rsc.sh
@@ -6,16 +6,62 @@
 #
 
 profile=$1
+ec2Comm="aws ec2 --profile ${profile} "
 
-aws ec2 describe-vpcs --profile ${profile} |grep "VpcId" |awk -F ":" '{print $2}' |tr -d [\",] >/tmp/vpc-ids.$$
+for vpc in $(${ec2Comm} describe-vpcs |grep "VpcId" |awk -F ":" '{print $2}' |tr -d [\",])
+do
+dt=$(date +%m%d%y)
+vpcInfo=account-${vpc}.info.list.${dt}
+echo "Date|VPC|InstanceID|LaunchTime|InstanceType|VolumeIds|VolumeType|Iops|Size|AZ" > ${vpcInfo}
 
-for vpc in $(cat /tmp/vpc-ids.$$); do
+	for instance in $(${ec2Comm} describe-instances --filters "Name=vpc-id,Values=${vpc}" --query "Reservations[*].Instances[*].[InstanceId]" |tr -d [\[,\]\"] | awk 'NF > 0')
+	do
 
-echo VPC:${vpc}
-
-aws ec2 describe-instances --filters "Name=vpc-id,Values=${vpc}" --profile ${profile} |grep InstanceId|awk -F \" '{print $4}' >/tmp/instance-ids.${vpc}.$$
-
-cat /tmp/instance-ids.${vpc}.$$
-
-
+	iLT=$(${ec2Comm} describe-instances --filters "Name=instance-id,Values=${instance}" --query "Reservations[*].Instances[*].[LaunchTime]" |tr -d [\[,\]\"] | awk 'NF > 0')
+	
+	iType=$(${ec2Comm} describe-instances --filters "Name=instance-id,Values=${instance}" --query "Reservations[*].Instances[*].[InstanceType]" |tr -d [\[,\]\"] | awk 'NF > 0')
+	
+	
+		#echo "Volumes:" $(${ec2Comm} describe-instances --filters "Name=instance-id,Values=${instance}" --query "Reservations[*].Instances[*].[BlockDeviceMappings[*].Ebs.VolumeId]" |tr -d [\[,\]] | awk 'NF > 0') >> ${instVolInfo}
+	
+		for vol in $(${ec2Comm} describe-instances --filters "Name=instance-id,Values=${instance}" --query "Reservations[*].Instances[*].[BlockDeviceMappings[*].Ebs.VolumeId]" |tr -d [\[,\]\"] | awk 'NF > 0')
+		do
+		
+		vType=$(${ec2Comm} describe-volumes --volume-id ${vol} --query 	"Volumes[*].[VolumeType]" |tr -d [\[,\]\"] | awk 'NF > 0')
+		vIops=$(${ec2Comm} describe-volumes --volume-id ${vol} --query 	"Volumes[*].[Iops]" |tr -d [\[,\]\"] | awk 'NF > 0')
+		vSize=$(${ec2Comm} describe-volumes --volume-id ${vol} --query 	"Volumes[*].[Size]" |tr -d [\[,\]\"] | awk 'NF > 0')
+		vAZ=$(${ec2Comm} describe-volumes --volume-id ${vol} --query 	"Volumes[*].[AvailabilityZone]" |tr -d [\[,\]\"] | awk 'NF > 0')
+		
+		echo "${dt}|${vpc}|${instance}|${iLT}|${iType}|${vol}|${vType}|${vIops}|${vSize}|${vAZ}|" >> ${vpcInfo} 	
+		
+		done
+		
+	done
+echo VPC: ${vpc}
+echo -----------------------------------------------------
+cat ${vpcInfo}  | tr -d " " >> vpcs.consolidated.info
+echo -----------------------------------------------------
 done; 
+
+
+
+##aws ec2 describe-instances --filters "Name=vpc-id,Values=vpc-df0e2fb7" --profile ${profile} --query "Reservations[*].Instances[*].[InstanceId,ImageId,LaunchTime,InstanceType,BlockDeviceMappings[*].Ebs.VolumeId]"
+#
+#aws ec2 describe-instances --filters "Name=vpc-id,Values=vpc-df0e2fb7" --profile ${profile} --query "Reservations[*].Instances[*].[InstanceId]" |tr -d [\[,\]] | awk 'NF > 0'
+#
+#
+#Find StartTime
+#aws ec2 describe-instances --filters "Name=instance-id,Values=i-0f3064aac7777a8a9" --profile ${profile} --query "Reservations[*].Instances[*].[LaunchTime]" |tr -d [\[,\]] | awk 'NF > 0' 
+#
+#Find InstanceType
+#aws ec2 describe-instances --filters "Name=instance-id,Values=i-0f3064aac7777a8a9" --profile ${profile} --query "Reservations[*].Instances[*].[InstanceType]" |tr -d [\[,\]] | awk 'NF > 0'
+#
+#Find Volumes
+#aws ec2 describe-instances --filters "Name=instance-id,Values=i-0f3064aac7777a8a9" --profile ${profile} --query "Reservations[*].Instances[*].[BlockDeviceMappings[*].Ebs.VolumeId]" |tr -d [\[,\]] | awk 'NF > 0'
+#
+#aws ec2 describe-volumes --volume-id vol-0872c2dd50d006bb5 --profile ${profile} --query "Volumes[*].[VolumeType]" |tr -d [\[,\]] | awk 'NF > 0'
+#aws ec2 describe-volumes --volume-id vol-0872c2dd50d006bb5 --profile ${profile} --query 	"Volumes[*].[Iops]" |tr -d [\[,\]] | awk 'NF > 0'
+#aws ec2 describe-volumes --volume-id vol-0872c2dd50d006bb5 --profile ${profile} --query 	"Volumes[*].[Size]" |tr -d [\[,\]] | awk 'NF > 0' 
+#aws ec2 describe-volumes --volume-id vol-0872c2dd50d006bb5 --profile ${profile} --query 	"Volumes[*].[AvailabilityZone]" |tr -d [\[,\]] | awk 'NF > 0'
+
+
